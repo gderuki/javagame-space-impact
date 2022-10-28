a43ce58851e7fbfc7d1b7eeba8c2d573fdccd42f
Pavels Sanatovs <psanatovs@gmail.com>
2022-10-28 19:32:20 +0300

    Added random pickups

--- core/src/sg/ebacorp/spaceimpact/FirstLevelScreen.java
+++ core/src/sg/ebacorp/spaceimpact/FirstLevelScreen.java
@@ -37,7 +37,7 @@ public class FirstLevelScreen implements Screen {
     Array<Rectangle> projectiles;
     Array<Rectangle> obstacles;
     long lastEnemySpawnTime;
-//    long lastExtraLifeSpawnTime;
+    //    long lastExtraLifeSpawnTime;
     long lastProjectileSpawnTime;
     long lastObstacleSpawnTime;
     long lastRespawnTime;
@@ -59,11 +59,11 @@ public class FirstLevelScreen implements Screen {
     private final int PLAYER_MOVE_SPEED = 130;
     // The more value -> the fewer enemies are being spawned
     @SuppressWarnings("FieldCanBeLocal")
-    private final long ENEMY_SPAWN_RATE_COMPARATOR = (100000000L);
+    private final long ENEMY_SPAWN_RATE_COMPARATOR = 16000000000L * 4;
     @SuppressWarnings("FieldCanBeLocal")
-    private final long RANDOM_PICKUP_SPAWN_RATE_COMPARATOR = (300000000L * 2L);
+    private final long RANDOM_PICKUP_SPAWN_RATE_COMPARATOR = 32000000000L * 8;
     @SuppressWarnings("FieldCanBeLocal")
-    private final long OBSTACLE_SPAWN_RATE_COMPARATOR = (500000000L * 2L);
+    private final long OBSTACLE_SPAWN_RATE_COMPARATOR = 24000000000L * 8;
     // The more value -> the fewer enemies are being spawned
     @SuppressWarnings("FieldCanBeLocal")
     private final long PROJECTILE_SPAWN_RATE_COMPARATOR = 180000000; // og: 1000000000
@@ -74,10 +74,27 @@ public class FirstLevelScreen implements Screen {
     @SuppressWarnings("FieldCanBeLocal")
     private final int TOP_BAR_OFFSET = (SCREEN_HEIGHT - 64 - 16);
     private int randomLUTCounter = 0;
+    // make it DOOM
     private final int[] randomLUT = {
-            18, 206, 50, 207, 167, 161, 13, 94, 81, 231, 256, 217, 71, 38, 183, 40, 220, 203, 249, 255, 237, 151, 170,
-            181, 133, 113, 103, 253, 16, 101, 158, 27, 146, 123, 204, 147, 229, 117, 97, 85, 246, 48, 216, 9, 99, 148,
-            108, 6, 177, 248, 86, 96, 14, 106, 199, 115, 184, 157, 64, 89, 5, 137, 74, 143, 21, 196
+            0, 8, 109, 220, 222, 241, 149, 107, 75, 248, 254, 140, 16, 66,
+            74, 21, 211, 47, 80, 242, 154, 27, 205, 128, 161, 89, 77, 36,
+            95, 110, 85, 48, 212, 140, 211, 249, 22, 79, 200, 50, 28, 188,
+            52, 140, 202, 120, 68, 145, 62, 70, 184, 190, 91, 197, 152, 224,
+            149, 104, 25, 178, 252, 182, 202, 182, 141, 197, 4, 81, 181, 242,
+            145, 42, 39, 227, 156, 198, 225, 193, 219, 93, 122, 175, 249, 0,
+            175, 143, 70, 239, 46, 246, 163, 53, 163, 109, 168, 135, 2, 235,
+            25, 92, 20, 145, 138, 77, 69, 166, 78, 176, 173, 212, 166, 113,
+            94, 161, 41, 50, 239, 49, 111, 164, 70, 60, 2, 37, 171, 75,
+            136, 156, 11, 56, 42, 146, 138, 229, 73, 146, 77, 61, 98, 196,
+            135, 106, 63, 197, 195, 86, 96, 203, 113, 101, 170, 247, 181, 113,
+            80, 250, 108, 7, 255, 237, 129, 226, 79, 107, 112, 166, 103, 241,
+            24, 223, 239, 120, 198, 58, 60, 82, 128, 3, 184, 66, 143, 224,
+            145, 224, 81, 206, 163, 45, 63, 90, 168, 114, 59, 33, 159, 95,
+            28, 139, 123, 98, 125, 196, 15, 70, 194, 253, 54, 14, 109, 226,
+            71, 17, 161, 93, 186, 87, 244, 138, 20, 52, 123, 251, 26, 36,
+            17, 46, 52, 231, 232, 76, 31, 221, 84, 37, 216, 165, 212, 106,
+            197, 242, 98, 43, 39, 175, 254, 145, 190, 84, 118, 222, 187, 136,
+            120, 163, 236, 249
     };
     private final boolean IS_DEBUG = RuntimeConfig.getInstance().isDebug;
 
@@ -229,11 +246,8 @@ public class FirstLevelScreen implements Screen {
 
     private int getNextRandomInteger() {
         // Ssanity check
-        if (randomLUTCounter >= 63) {
-            randomLUTCounter = 0;
-        }
-
-        return randomLUT[++randomLUTCounter];
+        randomLUTCounter = (randomLUTCounter + 1) & 0xff;
+        return randomLUT[randomLUTCounter];
     }
 
 
@@ -387,35 +401,34 @@ public class FirstLevelScreen implements Screen {
         }
 
         // draw power-ups/pick-ups here
-        if (lastPickedUpItemType == 1) {
-            if (xRaysCount > 0) {
-                game.batch.draw(
-                        xRayImage, // 64x64
-                        16 + 64 + 64 + 64 + 64 + 64,
-                        TOP_BAR_OFFSET
-                );
-                game.font.draw(
-                        game.batch,
-                        String.valueOf(xRaysCount),
-                        16 + 64 + 64 + 64 + 64 + 64 + 64,
-                        TOP_BAR_OFFSET + 62 // consider having 60 here...
-                );
-            }
-        } else if (lastPickedUpItemType == 2) {
-            if (shockWaveCount > 0) {
-                game.batch.draw(
-                        shockWaveImage, // 64x64
-                        16 + 64 + 64 + 64 + 64 + 64,
-                        TOP_BAR_OFFSET
-                );
-                game.font.draw(
-                        game.batch,
-                        String.valueOf(shockWaveCount),
-                        16 + 64 + 64 + 64 + 64 + 64 + 64,
-                        TOP_BAR_OFFSET + 62 // consider having 60 here...
-                );
-            }
+        if (xRaysCount > 0) {
+            game.batch.draw(
+                    xRayImage, // 64x64
+                    16 + 64 + 64 + 64 + 64 + 64,
+                    TOP_BAR_OFFSET
+            );
+            game.font.draw(
+                    game.batch,
+                    String.valueOf(xRaysCount),
+                    16 + 64 + 64 + 64 + 64 + 64 + 64,
+                    TOP_BAR_OFFSET + 62 // consider having 60 here...
+            );
         }
+
+//        if (shockWaveCount > 0) {
+//
+//            game.batch.draw(
+//                    shockWaveImage, // 64x64
+//                    16 + 64 + 64 + 64 + 64 + 64,
+//                    TOP_BAR_OFFSET
+//            );
+//            game.font.draw(
+//                    game.batch,
+//                    String.valueOf(shockWaveCount),
+//                    16 + 64 + 64 + 64 + 64 + 64 + 64,
+//                    TOP_BAR_OFFSET + 62 // consider having 60 here...
+//            );
+//        }
 
 
         // score
@@ -630,9 +643,10 @@ public class FirstLevelScreen implements Screen {
 //        xRays = new Array<>();
 
         // INFO: we don't reset other timers here, cause e.g. enemy timer is being reset inside `spawnEnemy()`
+        // natural delay
         lastProjectileSpawnTime = TimeUtils.nanoTime();
-//        lastExtraLifeSpawnTime = TimeUtils.nanoTime();
-        lastObstacleSpawnTime = TimeUtils.nanoTime(); // natural delay
+        lastRandomPickupSpawnTime = TimeUtils.nanoTime();
+        lastObstacleSpawnTime = TimeUtils.nanoTime();
 
         // init
         spawnEnemy();
