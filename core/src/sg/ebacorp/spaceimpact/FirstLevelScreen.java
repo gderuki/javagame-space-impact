0b350c1e578d151686659da9e936d2a2432a30db
Pavels Sanatovs <psanatovs@gmail.com>
2022-11-03 09:47:51 +0200

    Broken a random, added couple of TODOs, fixed obstacle overlapping, updated DrawUI(), now you can see no more than 3 xray powerup pickups

--- core/src/sg/ebacorp/spaceimpact/FirstLevelScreen.java
+++ core/src/sg/ebacorp/spaceimpact/FirstLevelScreen.java
@@ -59,44 +59,26 @@ public class FirstLevelScreen implements Screen {
     private final int PLAYER_MOVE_SPEED = 130;
     // The more value -> the fewer enemies are being spawned
     @SuppressWarnings("FieldCanBeLocal")
-    private final long ENEMY_SPAWN_RATE_COMPARATOR = 16000000000L * 4;
+    private final long ENEMY_SPAWN_RATE_COMPARATOR = 1000000000L;
     @SuppressWarnings("FieldCanBeLocal")
-    private final long RANDOM_PICKUP_SPAWN_RATE_COMPARATOR = 32000000000L * 8;
+    private final long RANDOM_PICKUP_SPAWN_RATE_COMPARATOR = 1000000000L;
     @SuppressWarnings("FieldCanBeLocal")
-    private final long OBSTACLE_SPAWN_RATE_COMPARATOR = 24000000000L * 8;
+    private final long OBSTACLE_SPAWN_RATE_COMPARATOR = 1000000000L;
     // The more value -> the fewer enemies are being spawned
     @SuppressWarnings("FieldCanBeLocal")
-    private final long PROJECTILE_SPAWN_RATE_COMPARATOR = 180000000; // og: 1000000000
+    private final long PROJECTILE_SPAWN_RATE_COMPARATOR = 200000000; // og: 1000000000
     // The more value -> the fewer enemies are being spawned
-    @SuppressWarnings("FieldCanBeLocal")
-    private final long XRAY_CANNON_SPAWN_RATE_COMPARATOR = 990000000; // og: 1000000000
     private ExecutionState executionState;
     @SuppressWarnings("FieldCanBeLocal")
     private final int TOP_BAR_OFFSET = (SCREEN_HEIGHT - 64 - 16);
     private int randomLUTCounter = 0;
     // make it DOOM
     private final int[] randomLUT = {
-            0, 8, 109, 220, 222, 241, 149, 107, 75, 248, 254, 140, 16, 66,
-            74, 21, 211, 47, 80, 242, 154, 27, 205, 128, 161, 89, 77, 36,
-            95, 110, 85, 48, 212, 140, 211, 249, 22, 79, 200, 50, 28, 188,
-            52, 140, 202, 120, 68, 145, 62, 70, 184, 190, 91, 197, 152, 224,
-            149, 104, 25, 178, 252, 182, 202, 182, 141, 197, 4, 81, 181, 242,
-            145, 42, 39, 227, 156, 198, 225, 193, 219, 93, 122, 175, 249, 0,
-            175, 143, 70, 239, 46, 246, 163, 53, 163, 109, 168, 135, 2, 235,
-            25, 92, 20, 145, 138, 77, 69, 166, 78, 176, 173, 212, 166, 113,
-            94, 161, 41, 50, 239, 49, 111, 164, 70, 60, 2, 37, 171, 75,
-            136, 156, 11, 56, 42, 146, 138, 229, 73, 146, 77, 61, 98, 196,
-            135, 106, 63, 197, 195, 86, 96, 203, 113, 101, 170, 247, 181, 113,
-            80, 250, 108, 7, 255, 237, 129, 226, 79, 107, 112, 166, 103, 241,
-            24, 223, 239, 120, 198, 58, 60, 82, 128, 3, 184, 66, 143, 224,
-            145, 224, 81, 206, 163, 45, 63, 90, 168, 114, 59, 33, 159, 95,
-            28, 139, 123, 98, 125, 196, 15, 70, 194, 253, 54, 14, 109, 226,
-            71, 17, 161, 93, 186, 87, 244, 138, 20, 52, 123, 251, 26, 36,
-            17, 46, 52, 231, 232, 76, 31, 221, 84, 37, 216, 165, 212, 106,
-            197, 242, 98, 43, 39, 175, 254, 145, 190, 84, 118, 222, 187, 136,
-            120, 163, 236, 249
+            6, 10, 5, 3, 4, 6, 10, 5, 4,
+            3, 4, 7, 9, 8, 2, 7, 9, 8,
+            2, 6, 10, 5, 3, 4, 7, 9, 8,
+            2, 7, 9, 8, 2, 6, 10, 5, 3
     };
-    private final boolean IS_DEBUG = RuntimeConfig.getInstance().isDebug;
 
     public FirstLevelScreen(final DefaultGame gam) {
         this.game = gam;
@@ -133,8 +115,6 @@ public class FirstLevelScreen implements Screen {
         item.width = 64;
         item.height = 64;
         // we add -> we clean on pickup, we repopulate
-        int randomInt = getNextRandomInteger();
-
         randomPickups.add(item);
         lastRandomPickupSpawnTime = TimeUtils.nanoTime();
     }
@@ -147,20 +127,6 @@ public class FirstLevelScreen implements Screen {
         obstacle.height = 64;
         obstacles.add(obstacle);
         lastObstacleSpawnTime = TimeUtils.nanoTime();
-    }
-
-    private void spawnXRayCannon() {
-        Rectangle item = new Rectangle();
-        item.x = SCREEN_WIDTH;
-        if (obstacles.size > 0) {
-            item.y = MathUtils.random(128, TOP_BAR_OFFSET - 64 - 16);
-        } else {
-            item.y = MathUtils.random(16, TOP_BAR_OFFSET - 64 - 16);
-        }
-        item.width = 64;
-        item.height = 64;
-        randomPickups.add(item);
-        lastRandomPickupSpawnTime = TimeUtils.nanoTime();
     }
 
     private void spawnEnemy() {
@@ -217,11 +183,8 @@ public class FirstLevelScreen implements Screen {
         // obstacles
         spawnObstacleWhenApplicable();
 
-        // extra life business
+        // random pickups spawn
         spawnRandomPickupWhenApplicable();
-
-        // powerful pickups yo
-//        spawnXRayCannonWhenApplicable();
 
         // bullets and enemies go here
         detectCollision();
@@ -230,34 +193,28 @@ public class FirstLevelScreen implements Screen {
     private void spawnObstacleWhenApplicable() {
         if (TimeUtils.nanoTime() - lastObstacleSpawnTime
                 >
-                (OBSTACLE_SPAWN_RATE_COMPARATOR * getNextRandomInteger())
+                (OBSTACLE_SPAWN_RATE_COMPARATOR * getNextRandomInteger(false))
         ) {
             spawnObstacle();
         }
     }
 
-    private void spawnXRayCannonWhenApplicable() {
-        if (TimeUtils.nanoTime() - lastRandomPickupSpawnTime
-                > XRAY_CANNON_SPAWN_RATE_COMPARATOR * getNextRandomInteger()
-        ) {
-            spawnXRayCannon();
-        }
-    }
-
-    private int getNextRandomInteger() {
+    private int getNextRandomInteger(boolean isDebug) {
         // Ssanity check
-        randomLUTCounter = (randomLUTCounter + 1) & 0xff;
-        return randomLUT[randomLUTCounter];
+        if (randomLUTCounter == randomLUT.length) {
+            randomLUTCounter = 0;
+        }
+        return isDebug ? 2 : randomLUT[randomLUTCounter];
     }
 
 
     private void spawnEnemyWhenApplicable() {
-        if (TimeUtils.nanoTime() - lastEnemySpawnTime > (ENEMY_SPAWN_RATE_COMPARATOR * getNextRandomInteger()))
+        if (TimeUtils.nanoTime() - lastEnemySpawnTime > (ENEMY_SPAWN_RATE_COMPARATOR * getNextRandomInteger(true)))
             spawnEnemy();
     }
 
     private void spawnRandomPickupWhenApplicable() {
-        if (TimeUtils.nanoTime() - lastRandomPickupSpawnTime > RANDOM_PICKUP_SPAWN_RATE_COMPARATOR * getNextRandomInteger()) {
+        if (TimeUtils.nanoTime() - lastRandomPickupSpawnTime > RANDOM_PICKUP_SPAWN_RATE_COMPARATOR * getNextRandomInteger(false)) {
             spawnPowerUp();
         }
     }
@@ -488,7 +445,7 @@ public class FirstLevelScreen implements Screen {
                 lastRespawnTime = TimeUtils.nanoTime();
                 // TODO: remember what I meant by next line xD
                 // TODO: make me work
-                if (livesCount > 1) {
+                if (livesCount >= 1) {
                     livesCount--;
                     if (TimeUtils.nanoTime() - lastRespawnTime > OBSTACLE_SPAWN_RATE_COMPARATOR) {
                         resetLevelState(true);
@@ -514,11 +471,11 @@ public class FirstLevelScreen implements Screen {
                 }
             }
 
-            for (Rectangle obstacle : obstacles) {
-                if (projectile.overlaps(obstacle)) {
-                    projectiles.removeIndex(projectiles.indexOf(projectile, false));
-                }
-            }
+//            for (Rectangle obstacle : obstacles) {
+//                if (projectile.overlaps(obstacle)) {
+//                    projectiles.removeIndex(projectiles.indexOf(projectile, false));
+//                }
+//            }
         }
 
         Iterator<Rectangle> iterEnemies = enemies.iterator();
@@ -550,18 +507,23 @@ public class FirstLevelScreen implements Screen {
 
             if (item.overlaps(player)) {
                 // todo update this to be random
-                int randomInt = getNextRandomInteger();
-                if (randomInt < 100) {
+                float mathRandom = MathUtils.random();
+                // TODO: if we have 3 lives -> don't spawn extra life
+                if (mathRandom < .4) {
                     if (livesCount <= 3) {
                         livesCount++;
                     }
                     lastPickedUpItemType = 0;
                     scoreCount += 40;
-                } else if (randomInt < 200) {
-                    xRaysCount++;
+                } else if (mathRandom > .4) {
+                    if (xRaysCount < 3) {
+                        xRaysCount++;
+                    }
+
                     lastPickedUpItemType = 1;
                     scoreCount += 80;
                 } else {
+                    // shock wave will spawn only if
                     shockWaveCount++;
                     lastPickedUpItemType = 2;
                     scoreCount += 120;
